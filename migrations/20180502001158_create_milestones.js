d5adbcde267aad9214ba309f5b7d89299c990bc5
samsamkim <hoonshik10@gmail.com>
2018-05-02 01:51:22 +0000

    database migration changes

---
+++ migrations/20180502001158_create_milestones.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+    return Promise.all([
+    knex.schema.createTable('milestones', function(table){
+      table.string('description');
+      table.date('date_achieved');
+      table.integer('id').notNullable().primary();
+    })
+  ])
+
+};
+
+exports.down = function(knex, Promise) {
+    return Promise.all([
+    knex.schema.dropTable('milestones')
+  ])
+
+};
