3f1d63ea7977252acdb671b018e09a8a7363e21b
samsamkim <hoonshik10@gmail.com>
2018-05-02 02:04:34 +0000

    milestones changed

--- migrations/20180502001158_create_milestones.js
+++ migrations/20180502001158_create_milestones.js
@@ -5,6 +5,7 @@ exports.up = function(knex, Promise) {
       table.string('description');
       table.date('date_achieved');
       table.integer('id').notNullable().primary();
+      table.integer('famous_person',11).unsigned().inTable('milestones').references('famous_person');
     })
   ])
 
